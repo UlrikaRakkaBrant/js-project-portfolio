3a941540dad489bcf2689feeecfdd09523370451
Ulrika Einebrant <einebrantaren@gmail.com>
2025-10-16 22:07:30 +0200

    chore(config): fix TypeScript/JS setup for styled-components and asset imports

---
+++ declarations.d.ts
@@ -0,0 +1,5 @@
+// declarations.d.ts
+declare module '*.svg' { const src: string; export default src; }
+declare module '*.png' { const src: string; export default src; }
+declare module '*.jpg' { const src: string; export default src; }
+declare module '*.jpeg' { const src: string; export default src; }
